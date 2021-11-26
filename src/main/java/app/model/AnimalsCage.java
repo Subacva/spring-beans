9023ed74ed217c8dc253e6a585fff17d8d7d8a8a
Vitaliy Ledovskoy <superpups2003@gmail.com>
2021-11-26 16:16:25 +0300

    qwe

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private Dog dog;
+    private Cat cat;
+    private Timer time;
+    public AnimalsCage(@Qualifier("cat") Cat cat,
+                       @Qualifier("dog") Dog dog,
+                       Timer time){
+        this.dog=dog;
+        this.cat=cat;
+        this.time=time;
+    }
 
-    @Autowired
-    private Animal animal;
-
-    public void whatAnimalSay() {
+    public void whatAnimalSay () {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(cat.toString());
+        System.out.println(dog.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());
         System.out.println("________________________");
+    }
+    public Timer getTimer(){
+        return time;
     }
 }
